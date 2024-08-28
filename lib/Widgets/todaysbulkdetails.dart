b8d7ff57d0d1b2af05c8e3f543224e8135399313
sajidh <sajidhshaji4@gmail.com>
2024-08-28 11:31:50 +0530

    in today's bulk deal section added button scroll to right feature

--- lib/Widgets/todaysbulkdetails.dart
+++ lib/Widgets/todaysbulkdetails.dart
@@ -1,103 +1,105 @@
+import 'package:bricksnmix/Controller/BulkDealsController.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import '../Screens/product_details_page.dart';
 import '../designs/ResponsiveInfo.dart';
 
 class Todaysbulkdetails extends StatelessWidget {
-
-  List<String>datas=['bulk1.png','bulk1.png'];
-
-
-   Todaysbulkdetails() ;
+  Todaysbulkdetails({super.key});
+  List<String> datas = ['bulk1.png', 'bulk1.png'];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Bulkdealscontroller bulkdealscontroller = Get.put(Bulkdealscontroller());
+    return SizedBox(
       width: double.infinity,
-      height: MediaQuery.of(context).size.width/2.3,
-
+      height: MediaQuery.of(context).size.width / 2.3,
       child: Column(
-
         children: [
-
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
-
             children: [
-
-              Padding(padding: EdgeInsets.all(ResponsiveInfo.isMobile(context)?8:12),
-
-                child:   Text("Today’s Bulk Deals",maxLines: 1,style: TextStyle(fontSize:ResponsiveInfo.isMobile(context)? 17:20,fontWeight: FontWeight.bold),
+              Padding(
+                padding:
+                    EdgeInsets.all(ResponsiveInfo.isMobile(context) ? 8 : 12),
+                child: Text(
+                  "Today’s Bulk Deals",
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontSize: ResponsiveInfo.isMobile(context) ? 17 : 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                   textAlign: TextAlign.center,
                 ),
               ),
-
-
-
-              Padding(padding: EdgeInsets.all(ResponsiveInfo.isMobile(context)?8:12),
-
-                  child: Image.asset('assets/homecategory/blackarrow.png',width: 30,height: 30,fit: BoxFit.fill,)
-              ),
-
-            ],
-
-          ),
-
-          MediaQuery.removePadding(context: context, child: Container(
-            width: double.infinity,
-            height: ResponsiveInfo.isMobile(context)?100:110,
-
-            child: GestureDetector(
-
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.fromLTRB(ResponsiveInfo.isMobile(context)?10:15, 0, ResponsiveInfo.isMobile(context)?10:15, 0),
-                children:
-
-                List.generate(
-                    datas.length,
-                        (i) => Padding(padding: EdgeInsets.fromLTRB(ResponsiveInfo.isMobile(context)?5:10, 0, ResponsiveInfo.isMobile(context)?5:10, 0),
-
-                      child:  Image.asset('assets/homecategory/'+datas[i],width: ResponsiveInfo.isMobile(context)? 237:250,
-                        height: ResponsiveInfo.isMobile(context)? 98 :105,fit: BoxFit.fill,) ,
-                    )
-
-
-
-
-
-
-
+              InkWell(
+                onTap: () {
+                  bulkdealscontroller.scrollToRight();
+                },
+                child: Padding(
+                  padding:
+                      EdgeInsets.all(ResponsiveInfo.isMobile(context) ? 8 : 12),
+                  child: Image.asset(
+                    'assets/homecategory/blackarrow.png',
+                    width: 30,
+                    height: 30,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              ) ,
-              onTap: (){
-
-                Navigator.push(context,
-                    MaterialPageRoute(builder:
-                        (context) =>
-                        ProductDetailsPage()
-                    )
-                );
-              },
-            )
-
-
-
-            ,
-          )
-            ,removeBottom: false,removeLeft: false,removeRight: false,removeTop: false,)
-
-
-
-
-
-
+              ),
+            ],
+          ),
+          MediaQuery.removePadding(
+            context: context,
+            removeBottom: false,
+            removeLeft: false,
+            removeRight: false,
+            removeTop: false,
+            child: SizedBox(
+              width: double.infinity,
+              height: ResponsiveInfo.isMobile(context) ? 100 : 110,
+              child: GestureDetector(
+                child: ListView(
+                  controller: bulkdealscontroller.scrollController,
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.fromLTRB(
+                    ResponsiveInfo.isMobile(context) ? 10 : 15,
+                    0,
+                    ResponsiveInfo.isMobile(context) ? 10 : 15,
+                    0,
+                  ),
+                  children: List.generate(
+                    datas.length,
+                    (i) => Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        ResponsiveInfo.isMobile(context) ? 5 : 10,
+                        0,
+                        ResponsiveInfo.isMobile(context) ? 5 : 10,
+                        0,
+                      ),
+                      child: Image.asset(
+                        'assets/homecategory/${datas[i]}',
+                        width: ResponsiveInfo.isMobile(context) ? 237 : 250,
+                        height: ResponsiveInfo.isMobile(context) ? 98 : 105,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ProductDetailsPage(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ],
-
       ),
-
-
     );
   }
 }
